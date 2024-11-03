902ac958885a75c2700d85ae69b8b45c82d22fa4
jenryEduardo <eduardogomezcalvo851@gmail.com>
2024-11-02 18:24:37 -0600

    modelo de doctor terminado

--- src/models/doctor.py
+++ src/models/doctor.py
@@ -1,19 +1,23 @@
 # doctor.py
 from sqlalchemy import Column, Integer, String
 from src.models.user import User, db
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Doctor(User):
-    __tablename__ = 'doctors'
+    schema_name = os.getenv('SCHEMA_NAME') 
+    __tablename__ = 'doctor'
+    __table_args__ = {'schema': schema_name}
 
-    # Definimos los campos específicos de Doctor
     id_doctor = Column(Integer, primary_key=True, autoincrement=True)
     especialidad = Column(String(50), nullable=False)
     num_licencia = Column(String(20), nullable=False)
 
     def __init__(self, nombre, apellido, email, contra, num_tel, rol, especialidad, num_licencia):
-        # Llamamos al constructor de la clase base User
+       
         super().__init__(nombre=nombre, apellido=apellido, email=email, contra=contra, num_tel=num_tel, rol=rol)
-        
-        # Asignamos los atributos específicos de Doctor
+       
         self.especialidad = especialidad
         self.num_licencia = num_licencia
--- src/routes/doctor.py
+++ src/routes/doctor.py
@@ -1,16 +1,22 @@
-from flask import Blueprint, request
-from src.controllers.doctorController import crear_doctor, obtener_doctor, eliminar_doctor
+from flask import Blueprint
+from src.controllers.doctorController import crear_doctor, login_doctor, obtener_doctor, eliminar_doctor
 
 doctor_blueprint = Blueprint('doctores', __name__)
 
 @doctor_blueprint.route('/doctors', methods=['POST'])
 def crear_doctor_ruta():
-    return crear_doctor()  # Llama a la función que maneja la creación de doctores
+    return crear_doctor()
 
-@doctor_blueprint.route('/doctors/<int:doctor_id>', methods=['GET'])
-def obtener_doctor_ruta(doctor_id):
-    return obtener_doctor(doctor_id)  # Llama a la función que obtiene los datos de un doctor
 
+@doctor_blueprint.route('/doctors/login', methods=['POST'])
+def login_doctor_ruta():
+    return login_doctor()
+
+@doctor_blueprint.route('/doctors/profile', methods=['GET'])
+def obtener_doctor_ruta():
+    return obtener_doctor()  
+
+# Ruta para eliminar un doctor por su ID
 @doctor_blueprint.route('/doctors/<int:doctor_id>', methods=['DELETE'])
 def eliminar_doctor_ruta(doctor_id):
-    return eliminar_doctor(doctor_id)  # Llama a la función que maneja la eliminación de un doctor
+    return eliminar_doctor(doctor_id)
